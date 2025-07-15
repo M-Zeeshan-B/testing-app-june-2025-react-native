84e014588c8e92b1ddccd584ec2651935fecd0c3
Zeeshan <zeeshi329@gmail.com>
2025-07-15 21:27:16 +0500

    feature: basic numeric flat list

--- src/screens/WhatsappStatus/WhatsappStatus.js
+++ src/screens/WhatsappStatus/WhatsappStatus.js
@@ -1,10 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Image,
+  ImageBackground,
+} from "react-native";
 import React from "react";
 import Entypo from "@expo/vector-icons/Entypo";
 import Fontisto from "@expo/vector-icons/Fontisto";
 import { styles } from "./Styles";
 
 export default function WhatsappStatus() {
+  const numericData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const _renderStatus = (item) => {
+    return (
+      <View style={styles.statusContainer}>
+        <ImageBackground
+          source={{
+            uri: "https://cdn.pixabay.com/photo/2020/01/15/12/57/kyrgyzstan-4767882_1280.jpg",
+          }}
+          style={styles.backgroundImageContainer}
+        >
+          <View style={styles.statusProfilePictureContainer}>
+            <Image
+              style={styles.statusProfilePicture}
+              source={require("../../../assets/764848.jpg")}
+            />
+          </View>
+          <View style={styles.statusNameContainer}>
+            <Text style={styles.statusName}>Dummy Name</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  };
+
   return (
     <View>
       <View style={styles.header}>
@@ -19,6 +51,17 @@ export default function WhatsappStatus() {
         </View>
       </View>
       <Text style={styles.statusHeaderText}>Status</Text>
+      <FlatList
+        data={numericData}
+        horizontal={true}
+        renderItem={({ item }) => _renderStatus(item)}
+      />
+      <View style={styles.channelRow}>
+        <Text style={styles.statusHeaderText}>Channels</Text>
+        <View style={styles.exploreContainer}>
+          <Text style={styles.exploreText}>Explore</Text>
+        </View>
+      </View>
     </View>
   );
 }
