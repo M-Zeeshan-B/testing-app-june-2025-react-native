d9b1887c576e49bb376a189eca8f9ff15e504381
Zeeshan <zeeshi329@gmail.com>
2025-07-23 20:52:44 +0500

    feature : confetti controls

--- src/components/ConfettiButton.js
+++ src/components/ConfettiButton.js
@@ -5,15 +5,28 @@ import ConfettiCannon from "react-native-confetti-cannon";
 export default function ConfettiButton() {
   const confettiRef = useRef();
   const boxRef = useRef();
-  const onButtonPress = () => {
+  const onStartPress = () => {
     if (confettiRef) confettiRef.current.start();
   };
+  const onStopPress = () => {
+    if (confettiRef) confettiRef.current.stop();
+  };
+  const onResumePress = () => {
+    if (confettiRef) confettiRef.current.resume();
+  };
+
   return (
-    <View ref={boxRef}>
-      <Button onPress={onButtonPress} title="Cheee tha" />
+    <View ref={boxRef} style={styles.buttonBox}>
+      <Button onPress={onStartPress} title="Cheee tha start" />
       <ConfettiCannon ref={confettiRef} count={200} origin={{ x: -10, y: 0 }} />
+      <Button onPress={onStopPress} title="Cheee tha stop" />
+      <Button onPress={onResumePress} title="Cheee tha resume" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonBox: {
+    gap: 10,
+  },
+});
